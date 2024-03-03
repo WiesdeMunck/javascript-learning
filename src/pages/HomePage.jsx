1d240fb43366e41d3ad7cd373b358dccf1ce2a50
Wies Munck <wies-wies@hotmail.com>
2024-03-03 21:23:47 +0100

    gsap animation ball bbouncing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,15 @@
 
 import React from "react";
 import NumberOne from "../components/NumberOne";
+import GsapAnimation from "../components/GsapAnimation/GsapAnimation";
 
 export default function HomePage() {
 	return (
 		<div>
 			<h1>Home</h1>
-			<NumberOne />
+			<GsapAnimation />
+
+			{/* <NumberOne /> */}
 		</div>
 	)
 }
