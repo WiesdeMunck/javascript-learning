f44a4bda4925ded582e72d95447dbf44fde3bcf2
WiesdeMunck <wies-wies@hotmail.com>
2024-03-11 16:09:16 +0100

    swiper slider

---
+++ src/components/SwiperAnimation/SwiperAnimation.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+
+export default function SwiperAnimation() {
+	return (
+		<section className="grid-item ">
+			<Swiper
+				className="swiper-animation"
+				spaceBetween={50}
+				slidesPerView={2}
+				onSlideChange={() => console.log('slide change')}
+				onSwiper={(swiper) => console.log(swiper)}
+			>
+				<SwiperSlide className="swiper-animation__content-wrapper">
+					<a href="/">
+						{' '}
+						<h2 className="swiper-animation__title">kitten 1</h2>
+						<div className="swiper-animation__img-wrapper">
+							{' '}
+							<img
+								className="swiper-animation__img"
+								src="/img/kitten1.png"
+								alt="A cute kitten"
+							/>
+						</div>
+					</a>
+				</SwiperSlide>
+				<SwiperSlide className="swiper-animation__content-wrapper">
+					<h2 className="swiper-animation__title">kitten 2</h2>
+					<div className="swiper-animation__img-wrapper">
+						{' '}
+						<img
+							className="swiper-animation__img"
+							src="/img/kitten2.png"
+							alt="A cute kitten"
+						/>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide className="swiper-animation__content-wrapper">
+					<h2 className="swiper-animation__title">kitten 3</h2>
+					<div className="swiper-animation__img-wrapper">
+						{' '}
+						<img
+							className="swiper-animation__img"
+							src="/img/kitten3.png"
+							alt="A cute kitten"
+						/>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide className="swiper-animation__content-wrapper">
+					<h2 className="swiper-animation__title">kitten 4</h2>
+					<div className="swiper-animation__img-wrapper">
+						{' '}
+						<img
+							className="swiper-animation__img"
+							src="/img/kitten4.png"
+							alt="A cute kitten"
+						/>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide className="swiper-animation__content-wrapper">
+					<h2 className="swiper-animation__title">kitten 5</h2>
+					<div className="swiper-animation__img-wrapper">
+						{' '}
+						<img
+							className="swiper-animation__img"
+							src="/img/kitten5.png"
+							alt="A cute kitten"
+						/>
+					</div>
+				</SwiperSlide>
+			</Swiper>
+		</section>
+	);
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,13 @@
 import React from "react";
 import NumberOne from "../components/NumberOne";
 import GsapAnimation from "../components/GsapAnimation/GsapAnimation";
+import SwiperAnimation from "../components/SwiperAnimation/SwiperAnimation";
 
 export default function HomePage() {
 	return (
 		<div>
 			<h1>Home</h1>
-			<GsapAnimation />
+			<SwiperAnimation />
 
 			{/* <NumberOne /> */}
 		</div>
