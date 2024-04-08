a811c139996441da10404432cc9e646da0c0b0f2
Wies Munck <wies-wies@hotmail.com>
2024-04-08 22:51:03 +0200

    finished awesome hover efect for meester

--- src/components/ProjectsHover/ProjectsHover.jsx
+++ src/components/ProjectsHover/ProjectsHover.jsx
@@ -4,32 +4,33 @@ export default function ProjectsHover() {
 
 
 	return (
-		<section key={1} className="grid-item --center-grid-item ">
+		<section key={1} className=" ">
 
 			<ul className='projects-hover'>
-				<li className="projects-hover__item">
-					<article className="projects-hover__article">
-						<div className='projects-hover__titles-wrapper'>
-							<h2 className="article projects-hover__item-title">title h2</h2>
-						</div>
-						<div className='projects-hover__titles-wrapper'>
-							<span className="article projects-hover__item-subtitle">subtitle me span</span>
-						</div>
-
-						<figure className="projects-hover__item-img-wrapper">
-							<img src="/img/kitten1.png" alt="kitten me" className="article projects-hover__item-img" />
-							<div className="projects-hover__img-titles-container">
-								<div className='projects-hover__img-titles-wrapper'>
-									<span className="projects-hover__item-img-title">title h2</span>
-								</div>
-								<div className='projects-hover__img-titles-wrapper'>
-									<span className="projects-hover__item-img-subtitle">subtitle me span</span>
-								</div>
+				{[...Array(6)].map((_, i) => (
+					<li className="projects-hover__item">
+						<div className="one"></div>
+						<div className="one"></div>
+						<article className="projects-hover__article">
+							<div className='projects-hover__titles-wrapper'>
+								<h2 className="article projects-hover__item-title">title h2</h2>
 							</div>
-						</figure>
+							<div className='projects-hover__titles-wrapper'>
+								<span className="article projects-hover__item-subtitle">subtitle me span</span>
+							</div>
 
-					</article>
-				</li>
+							<figure className="projects-hover__item-img-wrapper">
+								<img src="/img/kitten1.png" alt="kitten me" className="article projects-hover__item-img" />
+								<div className="projects-hover__img-titles-container">
+									<div className='projects-hover__img-titles-wrapper'>
+										<span className="projects-hover__item-img-title">title h2</span>
+									</div>
+								</div>
+							</figure>
+
+						</article>
+					</li>
+				))}
 			</ul>
 
 		</section>
