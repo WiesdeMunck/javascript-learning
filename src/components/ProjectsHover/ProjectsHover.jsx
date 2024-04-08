31f88db67063c87c162491affe902b5bd5777aaf
Wies Munck <wies-wies@hotmail.com>
2024-04-08 22:51:28 +0200

    new header

--- src/components/ProjectsHover/ProjectsHover.jsx
+++ src/components/ProjectsHover/ProjectsHover.jsx
@@ -13,17 +13,17 @@ export default function ProjectsHover() {
 						<div className="one"></div>
 						<article className="projects-hover__article">
 							<div className='projects-hover__titles-wrapper'>
-								<h2 className="article projects-hover__item-title">title h2</h2>
+								<h2 className="article projects-hover__item-title">title me</h2>
 							</div>
 							<div className='projects-hover__titles-wrapper'>
-								<span className="article projects-hover__item-subtitle">subtitle me span</span>
+								<span className="article projects-hover__item-subtitle">subtitle me</span>
 							</div>
 
 							<figure className="projects-hover__item-img-wrapper">
 								<img src="/img/kitten1.png" alt="kitten me" className="article projects-hover__item-img" />
 								<div className="projects-hover__img-titles-container">
 									<div className='projects-hover__img-titles-wrapper'>
-										<span className="projects-hover__item-img-title">title h2</span>
+										<span className="projects-hover__item-img-title">title me</span>
 									</div>
 								</div>
 							</figure>
